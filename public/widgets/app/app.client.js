da9bcbc24ff064400684e790e692bff42d8ad8a7
maddogawl <maddogawl@gmail.com>
2012-03-30 11:41:57 -0500

    Try port 81

--- public/widgets/app/app.client.js
+++ public/widgets/app/app.client.js
@@ -10,12 +10,6 @@ feather.ns("feathertutorials");
       onReady: function() {
         var me = this;
         me.get( ".topBar" ).effect("bounce", { times: 3 }, 300);
-      
-      //Load the inital content here, which is "What Is Feather"
-      //Then listen for events from the tutorialNav widget for examples
-      //and tutorials that need be displayed
-      
-
       }
     }
   });
